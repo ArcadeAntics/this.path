1e5405d0533b955a7dd67984ed8774b522feafed
Andrew Simmons <nsnjsnd@gmail.com>
2021-09-29 16:20:28 -0400

    Made changes for unix, prints nicer (kinda)

--- tests/Alphabetic_characters.R
+++ tests/Alphabetic_characters.R
@@ -1,46 +1,37 @@
-fun <- function(x = character()) {
-    cat(x, "", sep = "\n")
-    this.path::Rterm(c("--no-echo", "--no-restore", x), exprs = r"{
-        fun <- function(...) {
-            x <- c(...)
-            y <- encodeString(x, quote = "\"")
-            nc <- nchar(y, type = "width")
-            y <- paste0(y, strrep(" ", max(0L, nc) - nc))
-            i <- grepl("^[[:alpha:]]*$", x)
-            cat(y[ i], fill = TRUE, labels = c("alpha:",
-                format(as.character(seq_len(sum( i))), justify = "left", width = 6)))
-            cat("\n")
-            cat(y[!i], fill = TRUE, labels = c("not  :",
-                format(as.character(seq_len(sum(!i))), justify = "left", width = 6)))
-            cat("\n")
-        }
-        fun(as.character(parse(text = paste0("\"\\x", as.raw(1:255), "\""))),
+x <- as.character(str2expression(sprintf("\"\\x%02X\"", 33:255)))
+Encoding(x) <- "latin1"
+x <- enc2utf8(x)
+x <- c(x, "\u{03C3}", "\u{03B4}", "\u{7B90}", "\u{5316}", "\u{5B57}")
 
 
-            as.character(parse(text = paste0("\"\\u{", c("03C3", "03B4", "7B90", "5316", "5b57"), "}\""))))
-    }", quiet = TRUE)
+fun <- function (...)
+{
+    x <- c(...)
+    y <- encodeString(x, quote = "\"")
+    nc <- nchar(y, type = "width")
+    y <- paste0(y, strrep(" ", max(0L, nc) - nc))
+    i <- grepl("^[[:alpha:]]*$", x)
+    cat(y[ i], fill = TRUE, labels = c("alpha:",
+        format(as.character(seq_len(sum( i))), justify = "left", width = 6)))
+    cat("\n")
+    cat(y[!i], fill = TRUE, labels = c("not  :",
+        format(as.character(seq_len(sum(!i))), justify = "left", width = 6)))
+    cat("\n")
 }
 
 
-if (.Platform$OS.type == "windows") {
-    fun("LANG=da"     )
-    fun("LANG=de"     )
-    fun("LANG=en"     )
-    fun("LANG=en@quot")
-    fun("LANG=en_GB"  )
-    fun("LANG=es"     )
-    fun("LANG=fa"     )
-    fun("LANG=fr"     )
-    fun("LANG=it"     )
-    fun("LANG=ja"     )
-    fun("LANG=ko"     )
-    fun("LANG=lt"     )
-    fun("LANG=Meta"   )
-    fun("LANG=nn"     )
-    fun("LANG=pl"     )
-    fun("LANG=pt_BR"  )
-    fun("LANG=ru"     )
-    fun("LANG=tr"     )
-    fun("LANG=zh_CN"  )
-    fun("LANG=zh_TW"  )
-} else fun()
+fun2 <- function (..., LANGUAGE = list.dirs(system.file(package = "translations"), full.names = FALSE, recursive = FALSE))
+{
+    oLANGUAGE <- Sys.getenv("LANGUAGE", unset = NA)
+    if (is.na(oLANGUAGE))
+        on.exit(Sys.unsetenv("LANGUAGE"))
+    else on.exit(Sys.setenv(LANGUAGE = oLANGUAGE))
+    for (LANGUAGE in LANGUAGE) {
+        Sys.setenv(LANGUAGE = LANGUAGE)
+        cat("LANGUAGE=", LANGUAGE, "\n\n", sep = "")
+        fun(...)
+    }
+}
+
+
+fun2(x)
