cd8d98b5b89ff2c21979e52b1e8777b05c445447
Andrew Simmons <nsnjsnd@gmail.com>
2021-10-03 03:13:30 -0400

    Simplified input, moved REditor matrix to R folder

--- inst/extdata/save_R_Editor_translations.R
+++ inst/extdata/save_R_Editor_translations.R
@@ -1,39 +1,21 @@
-REditor <- matrix(dimnames = list(NULL, c(
-    "LANGUAGE", "Format: (* Custom Locale)"    , "locale"                      )), data = c(
-    "da"      , "Danish (Denmark)"             , "Danish_Denmark"              ,
-    "de"      , "German (Germany)"             , "German_Germany"              ,
-    "en"      , "English (World)"              , "English_World"               ,
-    "en@quot" , "English (World)"              , "English_World"               ,
-    "en_GB"   , "English (United Kingdom)"     , "English_United Kingdom"      ,
-    "es"      , "Spanish (Spain)"              , "Spanish_Spain"               ,
-    "fa"      , "Persian (Iran)"               , "Persian_Iran"                ,
-    "fr"      , "French (France)"              , "French_France"               ,
-    "it"      , "Italian (Italy)"              , "Italian_Italy"               ,
-    "ja"      , "Japanese (Japan)"             , "Japanese_Japan"              ,
-    "ko"      , "Korean (Korea)"               , "Korean_Korea"                ,
-    "lt"      , "Lithuanian (Lithuania)"       , "Lithuanian_Lithuania"        ,
-    "Meta"    , ""                             , ""                            ,
-    "nn"      , "Norwegian Nynorsk (Norway)"   , "Norwegian-Nynorsk_Norway"    ,
-    "pl"      , "Polish (Poland)"              , "Polish_Poland"               ,
-    "pt_BR"   , "Portuguese (Brazil)"          , "Portuguese_Brazil"           ,
-    "ru"      , "Russian (Russia)"             , "Russian_Russia"              ,
-    "tr"      , "Turkish (Turkey)"             , "Turkish_Turkey"              ,
-    "zh_CN"   , "Chinese (Simplified, China)"  , "Chinese (Simplified)_China"  ,
-    "zh_TW"   , "Chinese (Traditional, Taiwan)", "Chinese (Traditional)_Taiwan"
-), ncol = 3, byrow = TRUE)
+if (.Platform$OS.type != "windows")
+    stop("not windows")
 
 
+tmpRData <- tempfile(fileext = ".RData")
 FILE <- tempfile(fileext = ".rds")
-tryCatch(finally = unlink(FILE), {
+tryCatch({
+
+
     x <- matrix(dimnames = list(NULL, c(
         "LANGUAGE", "R Editor")), data = c(character()
-    ), nrow = 0, ncol = 2, byrow = TRUE)
+    ), nrow = 0L, ncol = 2L, byrow = TRUE)
 
 
     saveRDS(x, FILE)
 
 
-    writeLines(deparse(substitute({
+    this.path::delayedAssign2("expr", substitute({
         text <- names(utils::getWindowsHandles())[[2L]]
         if (Encoding(text) == "unknown")
             Encoding(text) <- "latin1"
@@ -43,23 +25,37 @@ tryCatch(finally = unlink(FILE), {
         x <- rbind(x, text, deparse.level = 0)
         saveRDS(x, FILE)
         q()
-    }, list(FILE = FILE))), "clipboard")
-    commands <- sprintf("Rgui --vanilla LANG=%1$s LANGUAGE=%1$s LC_COLLATE=%2$s LC_CTYPE=%2$s LC_MONETARY=%2$s, LC_TIME=%2$s",
-        format(this.path::commandQuote(REditor[, "LANGUAGE"])),
-        format(this.path::commandQuote(REditor[, "locale"])))
-    for (command in commands) this.path:::.system(command)
+    }, list(FILE = FILE)), evaluated = TRUE)
+    save(list = "expr", file = tmpRData, eval.promises = FALSE)
+
+
+    options <- c("--no-save", paste0("--workspace=", tmpRData),
+        "--no-site-file", "--no-init-file", "--no-environ")
+    rMouse::setAutoDelay(10)
+    for (language in rownames(this.path:::languages)) {
+        this.path:::Rgui(c(options, this.path:::languageEnvvars(language)), wait = FALSE)
+        rMouse::move( 15,  32)
+        rMouse::left()
+        rMouse::move(109,  77)
+        rMouse::left()
+        rMouse::move(340, 313)
+        rMouse::left()
+        rMouse::type("expr\n")
+    }
     x <- readRDS(FILE)
-})
 
 
-FILE <- this.path::here("R_Editor_translations.rds")
-if (file.exists(FILE)) {
-    y <- readRDS(FILE)
-    y <- y[!y[, "LANGUAGE"] %in% x[, "LANGUAGE"], , drop = FALSE]
-    x <- rbind(x, y)
-    x <- x[order(x[, "LANGUAGE"]), , drop = FALSE]
-}
-saveRDS(x, FILE)
+}, finally = unlink(c(tmpRData, FILE)))
 
 
-sys.source(this.path::here("save_R_Editor_regexp.R"), environment())
+# FILE <- this.path::here("R_Editor_translations.rds")
+# if (file.exists(FILE)) {
+#     y <- readRDS(FILE)
+#     y <- y[!y[, "LANGUAGE"] %in% x[, "LANGUAGE"], , drop = FALSE]
+#     x <- rbind(x, y)
+#     x <- x[order(x[, "LANGUAGE"]), , drop = FALSE]
+# }
+# saveRDS(x, FILE)
+#
+#
+# sys.source(this.path::here("save_R_Editor_regexp.R"), environment())
