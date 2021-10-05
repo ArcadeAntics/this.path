3db1e6ff5200ecc082838f26d77d2b8e6ad2e94a
Andrew Simmons <nsnjsnd@gmail.com>
2021-10-05 02:47:28 -0400

    Fixed minor issue when loading, unloading, and reloading the namespace

--- R/Hooks_for_Namespace_Events.R
+++ R/Hooks_for_Namespace_Events.R
@@ -1,14 +1,21 @@
 .onLoad <- function (libname, pkgname)
 {
-    if (is.na(toplevel <- Sys.getenv("R_THIS_PATH_TOPLEVEL", NA)))
+    if (is.na(otoplevel <<- Sys.getenv("R_THIS_PATH_TOPLEVEL", NA)))
         Sys.setenv(R_THIS_PATH_TOPLEVEL = TRUE)
-    else if (toplevel)
+    else if (otoplevel)
         Sys.setenv(R_THIS_PATH_TOPLEVEL = FALSE)
 }
+environment(.onLoad) <- new.env()
+evalq({
+    otoplevel <- NULL
+}, environment(.onLoad))
 
 
 .onUnload <- function (libpath)
 {
-    if (!is.na(Sys.getenv("R_THIS_PATH_TOPLEVEL", NA)))
+    if (is.na(otoplevel))
         Sys.unsetenv("R_THIS_PATH_TOPLEVEL")
+    else if (otoplevel)
+        Sys.setenv(R_THIS_PATH_TOPLEVEL = TRUE)
 }
+environment(.onUnload) <- environment(.onLoad)
