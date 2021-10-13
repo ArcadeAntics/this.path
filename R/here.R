7c3edbe60701ca6ca83c0a97e74f6c4870502070
Andrew Simmons <nsnjsnd@gmail.com>
2021-10-13 01:30:44 -0400

    Changes to accomodate URLs in this.path

--- R/here.R
+++ R/here.R
@@ -1,10 +1,12 @@
 here <- ici <- function (..., .. = 0L)
 {
-    base <- this.dir(verbose = FALSE)
-    # if (grepl("^(ftp|ftps|http|https)://", base))
-    #     base <- normalizeURL(paste(c(".", rep("..",
-    #         length.out = ..)), collapse = "/"), base)
-    # else
+    base <- this.path(verbose = FALSE)
+    if (grepl("^(ftp|ftps|http|https)://", base))
+        base <- .normalizeURL(paste(c(base, "..",
+            rep("..", length.out = ..)), collapse = "/"))
+    else {
+        base <- dirname(base)
         for (.. in seq_len(..)) base <- dirname(base)
+    }
     file.path(base, ...)
 }
