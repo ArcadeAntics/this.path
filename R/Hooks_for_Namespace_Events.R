5a6529aa2ae815df4a976575fc1c291f1604f307
Andrew Simmons <nsnjsnd@gmail.com>
2021-10-21 11:25:21 -0400

    Better syntax

--- R/Hooks_for_Namespace_Events.R
+++ R/Hooks_for_Namespace_Events.R
@@ -1,21 +1,19 @@
-.onLoad <- function (libname, pkgname)
+.onLoad <- evalq(envir = new.env(), function (libname, pkgname)
 {
     if (is.na(otoplevel <<- Sys.getenv("R_THIS_PATH_TOPLEVEL", NA)))
         Sys.setenv(R_THIS_PATH_TOPLEVEL = TRUE)
     else if (otoplevel)
         Sys.setenv(R_THIS_PATH_TOPLEVEL = FALSE)
-}
-environment(.onLoad) <- new.env()
-evalq({
+})
+evalq(envir = environment(.onLoad), {
     otoplevel <- NULL
-}, environment(.onLoad))
+})
 
 
-.onUnload <- function (libpath)
+.onUnload <- evalq(envir = environment(.onLoad), function (libpath)
 {
     if (is.na(otoplevel))
         Sys.unsetenv("R_THIS_PATH_TOPLEVEL")
     else if (otoplevel)
         Sys.setenv(R_THIS_PATH_TOPLEVEL = TRUE)
-}
-environment(.onUnload) <- environment(.onLoad)
+})
