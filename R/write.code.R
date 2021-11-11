88053823afb2160484be1e241ad518e06d27b5c0
Andrew Simmons <nsnjsnd@gmail.com>
2021-11-11 11:32:38 -0500

    added deparseCtrl, similar to source

--- R/write.code.R
+++ R/write.code.R
@@ -1,11 +1,15 @@
-write.code <- function (x, file = stdout(), evaluated = FALSE, simplify = !evaluated)
+write.code <- function (x, file = stdout(), evaluated = FALSE, simplify = !evaluated,
+    deparseCtrl = c("keepInteger", "showAttributes",
+        "useSource", "keepNA", "digits17"))
 {
     if (!evaluated)
         x <- substitute(x)
     x <- if (simplify && is.call(x) && x[[1]] == quote(`{`))
-        vapply(as.list(x[-1]), deparse1, width.cutoff = 60L,
-            collapse = "\n", FUN.VALUE = "")
-    else deparse1(x, width.cutoff = 60L, collapse = "\n")
+        vapply(as.list(x[-1]), deparse1, collapse = "\n",
+            width.cutoff = 60L, backtick = TRUE, control = deparseCtrl,
+            FUN.VALUE = "")
+    else deparse1(x, collapse = "\n", width.cutoff = 60L,
+        backtick = TRUE, control = deparseCtrl)
     if (is.null(file))
         x
     else {
