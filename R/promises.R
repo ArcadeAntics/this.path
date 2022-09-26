470bcf541b15e5d7972012b81fec1f002bdd0dd4
Andrew Simmons <nsnjsnd@gmail.com>
2022-09-26 05:50:57 -0400

    rearranged in.shell order to match .this.path order

--- R/promises.R
+++ R/promises.R
@@ -1,3 +1,8 @@
+# expressions that we need to evaluate for our code to work,
+# but which are NOT expected to change in a session,
+# so we can evaluate them exactly once when needed
+
+
 delayedAssign("os.unix"   , .Platform$OS.type == "unix")
 delayedAssign("os.windows", .Platform$OS.type == "windows")
 delayedAssign("os.macos"  , os.unix && capabilities("aqua"))
@@ -10,6 +15,6 @@ delayedAssign("gui.rstudio",               .Platform$GUI == "RStudio")
 delayedAssign("in.vscode"  , "tools:vscode" %in% search()            )
 
 
-delayedAssign("os.unix.in.shell"   , os.unix    &&                        "R" == basename(commandArgs()[[1L]])  && .Platform$GUI != "Tk" && !gui.rstudio && !in.vscode && !gui.aqua)
-delayedAssign("os.windows.in.shell", os.windows && grepl("(?i)^Rterm(\\.exe)?$", basename(commandArgs()[[1L]])) &&                          !gui.rstudio && !in.vscode && !gui.rgui)
+delayedAssign("os.unix.in.shell"   , os.unix    && !gui.rstudio && !in.vscode && !gui.aqua && .Platform$GUI != "Tk" &&                        "R" == basename(commandArgs()[[1L]]) )
+delayedAssign("os.windows.in.shell", os.windows && !gui.rstudio && !in.vscode && !gui.rgui                          && grepl("(?i)^Rterm(\\.exe)?$", basename(commandArgs()[[1L]])))
 delayedAssign("in.shell", os.unix.in.shell || os.windows.in.shell)
