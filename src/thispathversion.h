525466d087c8ab013c7ec53667636dc4d2d10069
Andrew Simmons <nsnjsnd@gmail.com>
2022-11-19 23:59:07 -0500

    fixed text docs for R.from.shell

--- src/thispathversion.h
+++ src/thispathversion.h
@@ -1,5 +1,5 @@
 #define THIS_PATH_MAJOR 1
 #define THIS_PATH_MINOR 0
 #define THIS_PATH_PATCH 2
-#define THIS_PATH_GIT   101
-#define THIS_PATH_BUILD 4
+#define THIS_PATH_GIT   102
+#define THIS_PATH_BUILD 5
