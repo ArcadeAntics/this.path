b0c48bfb844be8d73f3bb3b43d84f14045e974a7
Andrew Simmons <nsnjsnd@gmail.com>
2024-10-07 17:10:34 -0400

    with_script_path no longer loads package:box if not needed

--- R/enhances.R
+++ R/enhances.R
@@ -6,15 +6,43 @@ with_script_path <- function (expr, local = FALSE, n = 0L, envir = parent.frame(
     envir
     matchThisEnv
     srcfile
-    oscript_path <- box::script_path()
-    on.exit(box::set_script_path(oscript_path))
-    box::set_script_path(tryCatch3({
+    new_script_path <- tryCatch3({
         .External2(.C_this_path, local, envir, matchThisEnv, srcfile)
     }, thisPathNotFoundError = {
         if (is.null(wd <- getwd()))
             "."
         else path.join(wd, ".")
-    }))
+    })
+    if (isNamespaceLoaded("box")) {
+        oscript_path <- box::script_path()
+        on.exit(if (isNamespaceLoaded("box")) box::set_script_path(oscript_path))
+        box::set_script_path(new_script_path)
+    }
+    else {
+        set_script_path_expr <- NULL
+        delayedAssign("set_script_path_expr", {
+            oscript_path <- box::script_path()
+            on.exit(
+                if (isNamespaceLoaded("box")) box::set_script_path(oscript_path),
+                add = TRUE
+            )
+            box::set_script_path(new_script_path)
+            NULL
+        })
+        set_script_path_fun <- function(pkgname, pkgpath) set_script_path_expr
+        ## on exit, remove the hook from the list
+        hookName <- packageEvent("box")
+        on.exit({
+            hooks <- getHook(hookName)
+            for (i in seq_along(hooks)) {
+                if (.identical(hooks[[i]], set_script_path_fun)) {
+                    setHook(hookName, hooks[-i], "replace")
+                    break
+                }
+            }
+        })
+        setHook(hookName, set_script_path_fun)
+    }
     expr
 }
 
