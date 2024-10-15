7dc710aa137b3357a28766195dd0febbd124687b
Andrew Simmons <nsnjsnd@gmail.com>
2024-10-15 15:32:47 -0400

    improvements to with_script_path in case package:box is unloaded and reloaded

--- R/enhances.R
+++ R/enhances.R
@@ -13,55 +13,55 @@ with_script_path <- function (expr, local = FALSE, n = 0L, envir = parent.frame(
             "."
         else path.join(wd, ".")
     })
-    if (isNamespaceLoaded("box")) {
-        ## if the namespace is loaded, this is simple.
-        ##
-        ## get the original script path,
-        ## set the new script path,
-        ## and on exit set the script path back to the original script path
-        oscript_path <- box::script_path()
-        ## if 'expr' unloads the namespace, do not reload it in the on.exit
-        on.exit(if (isNamespaceLoaded("box")) box::set_script_path(oscript_path))
-        box::set_script_path(new_script_path)
-    }
-    else {
-        ## this makes R CMD check happy
-        set_script_path_expr <- NULL
-        ## make a promise that will do the same as the previous block
-        delayedAssign("set_script_path_expr", {
-            oscript_path <- box::script_path()
-            on.exit(
-                if (isNamespaceLoaded("box")) box::set_script_path(oscript_path),
-                add = TRUE
-            )
-            box::set_script_path(new_script_path)
-            ## value of the promise is NULL, not that it matters
-            NULL
-        })
-        ## a function that will evaluate the previous promise when called
-        set_script_path_fun <- function(pkgname, pkgpath) set_script_path_expr
-        ## append the function to the package:box onLoad hook list,
-        ## and on exit remove the hook from the list
-        ##
-        ## this leaves the hook list the same as it was before entering
-        ##
-        ## why use a hook? because we do not want to force package:box to load
-        ## the user will specifically choose when it is loaded
-        ##
-        ## so now, the code that relies on package:box being loaded is only run
-        ## after package:box has been loaded by the user's code
-        hookName <- packageEvent("box")
-        on.exit({
-            hooks <- getHook(hookName)
-            for (i in seq_along(hooks)) {
-                if (.identical(hooks[[i]], set_script_path_fun)) {
-                    setHook(hookName, hooks[-i], "replace")
-                    break
+
+
+    ## this makes R CMD check happy
+    set_script_path_expr <- NULL
+    ## a function that will make and evaluate a promise when called
+    eval.env <- environment()
+    first_time <- TRUE
+    set_script_path_fun <- function(pkgname, pkgpath) {
+        delayedAssign(
+            "set_script_path_expr",
+            {
+                oscript_path <- box::script_path()
+                ## first time only, set another on.exit to restore script_path
+                if (first_time) {
+                    on.exit(
+                        if (isNamespaceLoaded("box")) box::set_script_path(oscript_path),
+                        add = TRUE
+                    )
+                    first_time <- FALSE
                 }
-            }
-        })
-        setHook(hookName, set_script_path_fun)
+                box::set_script_path(new_script_path)
+                ## value of the promise is NULL, not that it matters
+                NULL
+            },
+            eval.env
+        )
+        set_script_path_expr
     }
+    ## append the function to the package:box onLoad hook list,
+    ## and on exit remove the hook from the list
+    ##
+    ## this leaves the hook list the same as it was before entering
+    ##
+    ## why use a hook? we do not want to force package:box to load, and we want
+    ## to re-run the function if the user reloads package:box
+    hookName <- packageEvent("box")
+    on.exit({
+        hooks <- getHook(hookName)
+        for (i in seq_along(hooks)) {
+            if (.identical(hooks[[i]], set_script_path_fun)) {
+                setHook(hookName, hooks[-i], "replace")
+                break
+            }
+        }
+    })
+    setHook(hookName, set_script_path_fun)
+    if (isNamespaceLoaded("box")) set_script_path_fun()
+
+
     expr
 }
 
