1e638c82a4e938af79c5d77aeb88e77d31167a40
ArcadeAntics <nsnjsnd@gmail.com>
2025-01-20 23:28:17 -0500

    remove defunct functions

--- R/defunct.R
+++ R/defunct.R
@@ -1,16 +1,5 @@
-## Defunct in this.path 2.4.0 (2024-02-16)
+## Defunct in <pkgname> <version> (<date>)
 
 
-set.sys.path.jupyter <- function (...)
-stop(.defunctError("set.jupyter.path", "@R_PACKAGE_NAME@", old = "set.sys.path.jupyter"))
-
-
-fileArgs <- function ()
-stop(.defunctError("progArgs", "@R_PACKAGE_NAME@", old = "fileArgs"))
-
-
-## Defunct in pkgname version (date)
-
-
-# old <- function (formals)
-# stop(.defunctError("new", "@R_PACKAGE_NAME@", old = "old"))
+# <old> <- function (<formals>)
+# stop(.defunctError("<new>", "@R_PACKAGE_NAME@", old = "<old>"))
