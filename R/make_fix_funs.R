5d464fe944014cc61b72e39246f4fe3aaa101e66
ArcadeAntics <nsnjsnd@gmail.com>
2025-01-25 23:55:04 -0500

    add (delayed = FALSE) to make_fix_funs()

--- R/make_fix_funs.R
+++ R/make_fix_funs.R
@@ -1,5 +1,5 @@
 path.functions <- .removeSource_from_inner_functions(
-                  function (file, local = FALSE, n = 0L, envir = parent.frame(n + 1L),
+                  function (file, delayed = FALSE, local = FALSE, n = 0L, envir = parent.frame(n + 1L),
     matchThisEnv = getOption("topLevelEnvironment"),
     srcfile = if (n) sys.parent(n) else 0L)
 {
@@ -7,6 +7,36 @@ path.functions <- .removeSource_from_inner_functions(
         n <- .External2(.C_asIntegerGE0, n)
         ofile <- .External2(.C_this_path, FALSE, TRUE, FALSE, FALSE, local, envir, matchThisEnv, srcfile)
         file <- .External2(.C_this_path, local, envir, matchThisEnv, srcfile)
+        directory <- .dir(file)
+        `attr<-`(environment(), "name", paste0("make_fix_funs:", file))
+    }
+    else if (delayed) {
+        ofile <- file
+        delayedAssign(
+            "file",
+            fun(),
+            evalq(envir = new.env(), {
+                if (.is_abs_path(ofile)) {
+                    fun <- function() {
+                        (set.sys.path(ofile, path.only = TRUE, allow.url = TRUE,
+                            allow.file.uri = TRUE))
+                    }
+                }
+                else {
+                    wd <- getwd()
+                    fun <- function() {
+                        owd <- getwd()
+                        on.exit(setwd(owd))
+                        setwd(wd)
+                        (set.sys.path(ofile, path.only = TRUE, allow.url = TRUE,
+                            allow.file.uri = TRUE))
+                    }
+                }
+                environment()
+            })
+        )
+        delayedAssign("directory", .dir(file))
+        `attr<-`(environment(), "name", paste0("make_fix_funs:", ofile))
     }
     else {
         ofile <- file
@@ -14,9 +44,10 @@ path.functions <- .removeSource_from_inner_functions(
             allow.file.uri = TRUE)
         ## remove the variable(s) created by set.sys.path()
         unset.sys.path()
+        directory <- .dir(file)
+        `attr<-`(environment(), "name", paste0("make_fix_funs:", file))
     }
-    rm(srcfile, matchThisEnv, envir, n, local)
-    directory <- .dir(file)
+    rm(srcfile, matchThisEnv, envir, n, local, delayed)
     this.path <- function(original = FALSE, contents = FALSE) {
         ## force the promise
         original
@@ -44,7 +75,6 @@ path.functions <- .removeSource_from_inner_functions(
     rel2here <- function(path) .relpath(path, directory, normalize = FALSE)
     rel2proj <- function(path) .relpath(path, .proj(directory), normalize = FALSE)
     LINENO <- function() .LINENO(file)
-    `attr<-`(environment(), "name", paste0("make_fix_funs:", file))
     lockEnvironment(environment(), bindings = TRUE)
     environment()
 }
